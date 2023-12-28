7bc94f5f911c6054d7efde2b8f201dcec4824a0c
YaBoiHunta <letoucan69420@gmail.com>
2023-12-27 16:02:05 -0800

    Add moveable skateboard feature

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Three js stuff. Learning how to build a portfolio using this. Teehee.
 Trying to build a skateboard shop looking 3D scene. Since thats what im intersted in. And then maybe i can have my skateboards explain the other projects or work ive done on things. 
 
 Just a idea, but im chipping away at it bit by bit.
+
+This branch will be a moveable skateboard that the user can move around the plane as if it were a skateboard park.
--- main.js
+++ main.js
@@ -3,16 +3,12 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Tween } from '@tweenjs/tween.js';
 
-const loader = new THREE.CubeTextureLoader();
-const texture = loader.load([
-    'right.png', // right
-    'left.png', // left
-    'top.png', // top
-    'bottom.png', // bottom
-    'front.png', // front
-    'back.png' // back
-]);
-
+// Global variables
+let moveForward = false;
+let moveBackward = false;
+let direction = 0; // The direction the skateboard is facing, in radians
+let speed = 0.1;
+let turnSpeed = 0.2; // How fast the skateboard turns
 
 // Set up the scene
 function setupScene() {
@@ -28,6 +24,16 @@ function setupCamera() {
     return camera;
 }
 
+// Light setup 
+function setupLights(scene) {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+}
+
 // Set up the renderer
 function setupRenderer() {
     const renderer = new THREE.WebGLRenderer();
@@ -37,27 +43,29 @@ function setupRenderer() {
     return renderer;
 }
 
-// Set up the controls
+// Set up the mouse controlers for the use to move around the screen with.
 function setupControls(camera, renderer) {
     const controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
     return controls;
 }
 
-// Set up the light
-function setupLight(scene) {
-    const light = new THREE.PointLight(0xffffff, 100.0); // To tell the computer that im using a HEX number, include that 0x in front of the color. 0xffffff is white. 1.0 is the intensity of the light.
-    light.position.set(1, 5, 5); // x, y, z position of the light.
-    light.castShadow = true;
-    scene.add(light);
-    return light;
+// Create the plane
+function createPlane(scene) {
+    const planeGeometry = new THREE.PlaneGeometry(100, 100, 1, 1);
+    const planeMaterial = new THREE.MeshStandardMaterial({ color: "red" });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    scene.add(plane);
 }
-
 
 // Create the skateboard mesh
 function createSkateboard(scene) {
     // Create the board
-    const boardGeometry = new THREE.BoxGeometry(1, 0.1, 0.3);
-    const boardMaterial = new THREE.MeshStandardMaterial({ color: "Red" });
+    const boardGeometry = new THREE.BoxGeometry(0.3, 0.1, 1); // Swapped the x and z dimensions
+    const boardMaterial = new THREE.MeshStandardMaterial({ color: "Yellow" });
     const board = new THREE.Mesh(boardGeometry, boardMaterial);
     board.position.y = 0.15;
     board.castShadow = true;
@@ -67,32 +75,30 @@ function createSkateboard(scene) {
     const wheelGeometry = new THREE.CylinderGeometry(0.05, 0.05, 0.1, 32);
     const wheelMaterial = new THREE.MeshStandardMaterial({ color: "green" });
     const wheel1 = new THREE.Mesh(wheelGeometry, wheelMaterial);
-    wheel1.position.set(-0.4, 0.05, 0.1);
-    wheel1.rotation.y = Math.PI / 2;
-    wheel1.rotation.z = Math.PI / 2;
-    
+    wheel1.position.set(0.1, 0.05, -0.4); // Swapped the x and z positions
+    wheel1.rotation.x = Math.PI / 2; // Rotated the wheel 90 degrees around the x-axis
     wheel1.castShadow = true;
     wheel1.receiveShadow = true;
 
     const wheel2 = wheel1.clone();
-    wheel2.position.x = 0.4;
+    wheel2.position.z = 0.4;
 
     const wheel3 = wheel1.clone();
-    wheel3.position.z = -0.1;
+    wheel3.position.x = -0.1;
 
     const wheel4 = wheel2.clone();
-    wheel4.position.z = -0.1;
+    wheel4.position.x = -0.1;
 
     // Create the trucks
     const truckGeometry = new THREE.BoxGeometry(0.02, 0.02, 0.2);
     const truckMaterial = new THREE.MeshStandardMaterial({ color: "silver" });
     const truck1 = new THREE.Mesh(truckGeometry, truckMaterial);
-    truck1.position.set(-0.4, 0.1, 0);
+    truck1.position.set(0, 0.1, -0.4); // Swapped the x and z positions
     truck1.castShadow = true;
     truck1.receiveShadow = true;
 
     const truck2 = truck1.clone();
-    truck2.position.x = 0.4;
+    truck2.position.z = 0.4;
 
     // Create a group and add the board, wheels, and trucks to it
     const skateboard = new THREE.Group();
@@ -108,143 +114,71 @@ function createSkateboard(scene) {
     return skateboard;
 }
 
-function createLampPost(scene) {
-    const postGeometry = new THREE.CylinderGeometry(.5, 1, 6);
-    const postMaterial = new THREE.MeshStandardMaterial({ color: "silver" });
-    const post = new THREE.Mesh(postGeometry, postMaterial);
-    post.position.y = 3;
-    post.position.z = 10;
-    post.castShadow = true;
-    post.receiveShadow = true;
-
-    // Create the lamp shade at the top of the post
-    const shadeGeometry = new THREE.TorusGeometry(0.2, 0.10, 16, 100);
-    const shadeMaterial = new THREE.MeshStandardMaterial({ color: "yellow", side: THREE.DoubleSide }); // Change the color here
-    const shade = new THREE.Mesh(shadeGeometry, shadeMaterial);
-    shade.position.y = 6.2; // Position the shade at the top of the post
-    shade.rotation.x = Math.PI / 2; // Rotate the shade to face upwards
-
-    // Create the light inside the lamp shade
-    const postLight = new THREE.PointLight(0xffffff, 1.0);
-    postLight.position.set(0, 6.2, 10); // Position the light inside the lamp shade
-    postLight.castShadow = true;
-
-    // Create a group and add the post, shade, and light to it
-    const lampPost = new THREE.Group();
-    lampPost.add(post);
-    lampPost.add(shade);
-    lampPost.add(postLight);
-
-    scene.add(lampPost);
-    return lampPost;
+// Update the direction based on the arrow keys
+function handleKeyDown(event) {
+    switch (event.key) {
+        case 'ArrowLeft':
+            direction += turnSpeed;
+            break;
+        case 'ArrowRight':
+            direction -= turnSpeed;
+            break;
+        case 'ArrowUp':
+            moveForward = true;
+            break;
+        case 'ArrowDown':
+            moveBackward = true;
+            break;
+    }
 }
 
-    
-
-
-// Create the cube mesh
-function createCube(scene) {
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    const material = new THREE.MeshStandardMaterial({ color: "red" });
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.y = 1;
-    cube.castShadow = true;
-    cube.receiveShadow = true;
-    scene.add(cube);
-    return cube;
-}
-// Create the torus mesh
-function createTorus(scene) {
-    const geometry = new THREE.TorusGeometry(3, .16, 16, 100);
-    const material = new THREE.MeshStandardMaterial({ color: "blue" });
-    const torus = new THREE.Mesh(geometry, material);
-    torus.position.y = 2;
-    torus.castShadow = true;
-    torus.receiveShadow = true;
-    scene.add(torus);
-    return torus;
+// Update the direction based on the arrow keys
+function handleKeyUp(event) {
+    switch (event.key) {
+        case 'ArrowUp':
+            moveForward = false;
+            break;
+        case 'ArrowDown':
+            moveBackward = false;
+            break;
+    }
 }
 
-// Create the gem mesh
-function createGem(scene) {
-    const geometry = new THREE.OctahedronGeometry(1);
-    const material = new THREE.MeshStandardMaterial({ color: "cyan" });
-    const gem = new THREE.Mesh(geometry, material);
-    gem.position.y = 6; // Adjust this value to place the gem on top of the torus
-    gem.castShadow = true;
-    gem.receiveShadow = true;
-    scene.add(gem);
-    return gem;
+// Update the skateboard's position
+function updateSkateboardPosition(skateboard) {
+    if (moveForward) {
+        skateboard.position.x -= speed * Math.sin(direction);
+        skateboard.position.z -= speed * Math.cos(direction);
+    }
+    if (moveBackward) {
+        skateboard.position.x += speed * Math.sin(direction);
+        skateboard.position.z += speed * Math.cos(direction);
+    }
+    skateboard.rotation.y = direction;
 }
 
-// Create the plane mesh
-function createPlane(scene) {
-    const planeGeometry = new THREE.PlaneGeometry(20, 20);
-    const planeMaterial = new THREE.MeshStandardMaterial({ color: "gray", side: THREE.DoubleSide });
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = Math.PI / 2;
-    plane.receiveShadow = true;
-    scene.add(plane);
-    return plane;
+// Initialize the scene, renderer, camera, controls, skateboard, and other elements
+function init() {
+    const scene = setupScene();
+    const renderer = setupRenderer();
+    const camera = setupCamera();
+    const controls = setupControls(camera, renderer);
+    const skateboard = createSkateboard(scene);
+    createPlane(scene);
+    setupLights(scene);
+
+    // Add event listeners for keydown and keyup events
+    window.addEventListener('keydown', handleKeyDown, false);
+    window.addEventListener('keyup', handleKeyUp, false);
+
+    // Render the scene by calling the animation function
+    function animate() {
+        requestAnimationFrame(animate);
+        updateSkateboardPosition(skateboard);
+        renderer.render(scene, camera);
+    }
+    animate();
 }
 
-
-// Create the rectangle mesh
-function createRectangle(scene, cube, plane) {
-    const rectangleGeometry = new THREE.BoxGeometry(10, 1, 4);
-    const rectangleMaterial = new THREE.MeshStandardMaterial({ color: "purple" });
-    const rectangle = new THREE.Mesh(rectangleGeometry, rectangleMaterial);
-    rectangle.position.y = cube.position.y + 1 / 2;
-    rectangle.position.z = plane.position.z;
-    scene.add(rectangle);
-    return rectangle;
-}
-
-
-// Update the position of the rectangle
-function updateRectanglePosition(rectangle, cube) {
-    const radius = 5;
-    const speed = .01;
-    const angle = performance.now() * speed;
-    rectangle.position.x = Math.cos(angle) * radius;
-    rectangle.position.z = Math.sin(angle) * radius;
-    rectangle.lookAt(cube.position);
-}
-
-// Set up the scene, camera, renderer, controls, light, cube, plane, and rectangle and other objects in the scene
-const scene = setupScene();
-const camera = setupCamera();
-const renderer = setupRenderer();
-const controls = setupControls(camera, renderer);
-const light = setupLight(scene);
-const cube = createCube(scene);
-const plane = createPlane(scene);
-const rectangle = createRectangle(scene, cube, plane);
-const torus = createTorus(scene);
-const gem = createGem(scene);
-const skateboard1 = createSkateboard(scene);
-skateboard1.position.set(-3, .5, 2);
-const skateboard2 = createSkateboard(scene);
-skateboard2.position.set(3, .5, 2);
-const skateboard3 = createSkateboard(scene);
-skateboard3.position.set(1, .5, 4);
-
-const lampPost = createLampPost(scene);
-
-
-
-
-// Handle the mouse click event
-window.addEventListener('click', (event) => {
-    onMouseClick(event, camera, cube);
-}, false);
-
-// Render the scene and update the position of the rectangle
-function render() {
-    requestAnimationFrame(render);
-    updateRectanglePosition(rectangle, cube);
-    renderer.render(scene, camera);
-}
-
-// Start rendering
-render();
+// Call the init function to start the application
+init();
